a19b4bb7bef74d0f0c81ccc55a811a1da74076cc
YanielRamos <yanielramos0504@gmail.com>
2023-05-11 16:54:28 +0000

    adding swap function

--- monty.h
+++ monty.h
@@ -46,4 +46,5 @@ void free_stack(stack_t *stack);
 void tokenize_and_execute(char *line, unsigned int line_number, stack_t **stack);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 #endif
--- more_functions.c
+++ more_functions.c
@@ -23,3 +23,24 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	free(temp);
 }
+
+/**
+ * swap - function that swap
+ * @stack: elements to swap
+ * @line_number: amount of char
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+	
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
